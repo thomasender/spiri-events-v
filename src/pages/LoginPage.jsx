666e409c2cc5d184cb6d78a28aaefe805aee4da2
Thomas Ender <freisingen@yahoo.com>
2026-03-25 17:23:35 +0100

    cleanup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,13 @@
+import { Navigate } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 import AuthForm from '../components/AuthForm'
 
 export default function LoginPage() {
+  const { user, loading } = useAuth()
+
+  if (loading) return <div className="loading-spinner"></div>
+
+  if (user) return <Navigate to="/" replace />
+
   return <AuthForm />
 }
